8903e63794b358b383a3bb2cf256a927464406a0
Carlos Gastelum <carlosgastelum9923@gmail.com>
2024-04-10 22:07:05 -0600

    agregando estructura en Home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ import NavBar from "../../components/navBar/navBar";
 const Home = (() => {
   return (
     <>
-    
+    <NavBar></NavBar>
     <div className="mt-20 grid grid-cols-4 gap-2">
     <CardNote/>
     <CardNote/>
